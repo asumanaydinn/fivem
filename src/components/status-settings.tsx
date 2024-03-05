0a8921740c2d03591b10a14fecf6d7ae3524866d
asumanaydinn <158189669+asumanaydinn@users.noreply.github.com>
2024-03-05 13:55:23 +0300

    clean code

--- src/components/status-settings.tsx
+++ src/components/status-settings.tsx
@@ -15,6 +15,7 @@ import { StatusStyle7 } from "./icons/status-style-7";
 import { StatusStyle8 } from "./icons/status-style-8";
 import { StatusStyle9 } from "./icons/status-style-9";
 import { StressIcon } from "./icons/stress-icon";
+
 import ListItem from "./settings-item";
 import StatusStyleSettingItem from "./status-style-setting-item";
 
@@ -42,72 +43,60 @@ const StatusSettings = () => {
           description="Off-On Settings"
           icon={<HealthIcon />}
           title="Hide Health"
-        />{" "}
+        />
         <ListItem
           handleToggle={() => toggleSetting("hideHydration")}
           isOn={settings.status.hideHydration}
           description="Off-On Settings"
           icon={<DehydrationIcon />}
           title="Hide Hydration"
-        />{" "}
+        />
         <ListItem
           handleToggle={() => toggleSetting("hideHungry")}
           isOn={settings.status.hideHungry}
           description="Off-On Settings"
           icon={<HungryIcon />}
           title="Hide Hungry"
-        />{" "}
+        />
         <ListItem
           handleToggle={() => toggleSetting("hideEnergy")}
           isOn={settings.status.hideEnergy}
           description="Off-On Energy"
           icon={<EnergyIcon />}
           title="Hide Energy"
-        />{" "}
+        />
         <ListItem
           handleToggle={() => toggleSetting("hideArmor")}
           isOn={settings.status.hideArmor}
           description="Off-On Settings"
           icon={<ArmorIcon />}
           title="Hide Armor"
-        />{" "}
+        />
         <ListItem
           handleToggle={() => toggleSetting("hideStress")}
           isOn={settings.status.hideStress}
           description="Off-On Settings"
           icon={<StressIcon />}
           title="Hide Stress"
-        />{" "}
+        />
       </div>
 
       <div className="grid grid-cols-3 gap-3 justify-between">
-        <StatusStyleSettingItem statusStyleNumber={1}>
-          <StatusStyle1 />
-        </StatusStyleSettingItem>
-        <StatusStyleSettingItem statusStyleNumber={2}>
-          <StatusStyle2 />
-        </StatusStyleSettingItem>
-        <StatusStyleSettingItem statusStyleNumber={3}>
-          <StatusStyle3 />
-        </StatusStyleSettingItem>
-        <StatusStyleSettingItem statusStyleNumber={4}>
-          <StatusStyle4 />
-        </StatusStyleSettingItem>
-        <StatusStyleSettingItem statusStyleNumber={5}>
-          <StatusStyle5 />
-        </StatusStyleSettingItem>
-        <StatusStyleSettingItem statusStyleNumber={6}>
-          <StatusStyle6 />
-        </StatusStyleSettingItem>
-        <StatusStyleSettingItem statusStyleNumber={7}>
-          <StatusStyle7 />
-        </StatusStyleSettingItem>{" "}
-        <StatusStyleSettingItem statusStyleNumber={8}>
-          <StatusStyle8 />
-        </StatusStyleSettingItem>{" "}
-        <StatusStyleSettingItem statusStyleNumber={9}>
-          <StatusStyle9 />
-        </StatusStyleSettingItem>
+        {[
+          StatusStyle1,
+          StatusStyle2,
+          StatusStyle3,
+          StatusStyle4,
+          StatusStyle5,
+          StatusStyle6,
+          StatusStyle7,
+          StatusStyle8,
+          StatusStyle9,
+        ].map((StatusStyle, index) => (
+          <StatusStyleSettingItem key={index} statusStyleNumber={index + 1}>
+            <StatusStyle />
+          </StatusStyleSettingItem>
+        ))}
       </div>
     </div>
   );
