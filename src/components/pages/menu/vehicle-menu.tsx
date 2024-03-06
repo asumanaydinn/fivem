9cb6a1c8c557d5126b9221ce6a5abf90270f6f2b
asumanaydinn <158189669+asumanaydinn@users.noreply.github.com>
2024-03-06 15:58:33 +0300

    Add neon view

--- src/components/pages/menu/vehicle-menu.tsx
+++ src/components/pages/menu/vehicle-menu.tsx
@@ -40,44 +40,86 @@ const VehicleMenu = () => {
       />
 
       <div className="flex items-center justify-center  gap-x-2">
-        <div className=" flex flex-col gap-y-2">
-          <div className="w-20 h-[23.23px] relative">
-            <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
-            <div className="left-[24.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
-              Bonnet
+        {selectedTab === "Doors" && (
+          <div className=" flex flex-col gap-y-2">
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[24.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                Bonnet
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[8.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                Left Front Door
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[5.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                Right Front Door
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[8.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                Left Back Door
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[6.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                Right Back Door
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[27.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                Trunk
+              </div>
             </div>
           </div>
-          <div className="w-20 h-[23.23px] relative">
-            <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
-            <div className="left-[8.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
-              Left Front Door
+        )}
+        {selectedTab === "Neons" && (
+          <div className="flex flex-col gap-y-2">
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[31.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                ALL
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[24.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                FRONT
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[28.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                LEFT
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[26.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                RIGHT
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[28.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                REAR
+              </div>
+            </div>
+            <div className="w-20 h-[23.23px] relative">
+              <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
+              <div className="left-[25.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
+                PARTY
+              </div>
             </div>
           </div>
-          <div className="w-20 h-[23.23px] relative">
-            <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
-            <div className="left-[5.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
-              Right Front Door
-            </div>
-          </div>
-          <div className="w-20 h-[23.23px] relative">
-            <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
-            <div className="left-[8.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
-              Left Back Door
-            </div>
-          </div>
-          <div className="w-20 h-[23.23px] relative">
-            <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
-            <div className="left-[6.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
-              Right Back Door
-            </div>
-          </div>
-          <div className="w-20 h-[23.23px] relative">
-            <div className="w-20 h-[23.23px] left-0 top-0 absolute bg-zinc-300 bg-opacity-5 rounded-sm border border-white border-opacity-25" />
-            <div className="left-[27.47px] top-[6.29px] absolute text-center text-neutral-400 text-[8.89px] font-semibold font-['Inter']">
-              Trunk
-            </div>
-          </div>
-        </div>
+        )}
         <div className="relative flex items-center justify-center">
           <div className="absolute">
             <CarBackground />
