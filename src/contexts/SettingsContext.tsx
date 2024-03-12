ab5a7fa00ec4fc8b27f0d3f90f2865ecdde92b1c
asumanaydinn <158189669+asumanaydinn@users.noreply.github.com>
2024-03-12 15:21:19 +0300

    Add speedometer 5 big

--- src/contexts/SettingsContext.tsx
+++ src/contexts/SettingsContext.tsx
@@ -50,7 +50,7 @@ const defaultSettings: AppSettings = {
   },
   speedometers: {
     units: "kmh",
-    speedometerType: "4",
+    speedometerType: "5",
   },
   status: {
     statusStyleType: "10",
