c9f73a2ed9eda1223d4851df7167e5f231f043a1
asumanaydinn <158189669+asumanaydinn@users.noreply.github.com>
2024-03-17 01:16:49 +0300

    bring back settings button

--- src/components/pages/hud/index.tsx
+++ src/components/pages/hud/index.tsx
@@ -89,6 +89,18 @@ const Hud = () => {
         alt="background"
       />
       <div className="p-4 w-full h-full">
+        <button
+          className="absolute top-4 left-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+          onClick={() => setIsModalOpen(true)}
+        >
+          Open Settings
+        </button>
+
+        <SettingsModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
+
         {!settings.general.hideAllHud && (
           <div className="relative w-full h-full">
             <ReactPlayer
@@ -125,18 +137,6 @@ const Hud = () => {
                 )}
               </div>
             </Draggable>
-
-            <button
-              className="absolute top-0 left-0 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
-              onClick={() => setIsModalOpen(true)}
-            >
-              Open Settings
-            </button>
-
-            <SettingsModal
-              isOpen={isModalOpen}
-              onClose={() => setIsModalOpen(false)}
-            />
 
             <Draggable disabled={!settings.general.freeformEditMode}>
               <div className="absolute flex flex-col gap-y-2 right-0 bottom-0">
