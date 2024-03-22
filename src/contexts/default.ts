42663c52aee6d69e58dba5a400738a678c94d44e
asumanaydinn <158189669+asumanaydinn@users.noreply.github.com>
2024-03-22 09:14:07 +0300

    speedometer 2 car settings

--- src/contexts/default.ts
+++ src/contexts/default.ts
@@ -1,136 +1,135 @@
 import { AppSettings } from "../types/settings";
 
 export const defaultSettings: AppSettings = {
-    general: {
-      cinematicMode: false,
-      showLocation: true,
-      minimap: true,
-      freeformEditMode: false,
-      hideAllHud: false,
-      hideUserMoney: false,
-      hideServerLogo: false,
-      hideQuickInfo: false,
-    },
-    speedometers: {
-      units: "kmh",
-      speedometerType: "7",
-    },
-    status: {
-      statusStyleType: "6",
-      hideAllStatus: false,
-      hideHealth: false,
-      hideHydration: false,
-      hideHungry: false,
-      hideEnergy: false,
-      hideArmor: false,
-      hideStress: false,
-    },
-    styleVisibility: {
-      "1": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
-      },
-      "2": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
-      },
-      "3": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
-      },
-      "4": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
-      },
-      "5": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
-      },
-      "6": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
-      },
-      "7": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
-      },
-      "8": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
-      },
-      "9": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
-      },
-      "10": {
-        visibilityThresholds: {
-          armor: 0,
-          energy: 0,
-          health: 0,
-          hunger: 0,
-          hydration: 0,
-          stress: 0,
-        },
+  general: {
+    cinematicMode: false,
+    showLocation: true,
+    minimap: true,
+    freeformEditMode: false,
+    hideAllHud: false,
+    hideUserMoney: false,
+    hideServerLogo: false,
+    hideQuickInfo: false,
+  },
+  speedometers: {
+    units: "kmh",
+    speedometerType: "2",
+  },
+  status: {
+    statusStyleType: "6",
+    hideAllStatus: false,
+    hideHealth: false,
+    hideHydration: false,
+    hideHungry: false,
+    hideEnergy: false,
+    hideArmor: false,
+    hideStress: false,
+  },
+  styleVisibility: {
+    "1": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
       },
     },
-    carSettings: {
-      seatBelts: "off",
-      engineHasProblem: false,
-      doorIsOpen: false,
+    "2": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
+      },
     },
-  };
-  
+    "3": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
+      },
+    },
+    "4": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
+      },
+    },
+    "5": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
+      },
+    },
+    "6": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
+      },
+    },
+    "7": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
+      },
+    },
+    "8": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
+      },
+    },
+    "9": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
+      },
+    },
+    "10": {
+      visibilityThresholds: {
+        armor: 0,
+        energy: 0,
+        health: 0,
+        hunger: 0,
+        hydration: 0,
+        stress: 0,
+      },
+    },
+  },
+  carSettings: {
+    seatBelts: "off",
+    engineHasProblem: false,
+    doorIsOpen: false,
+  },
+};
